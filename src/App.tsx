045d35569117890ca7c55a854c14f34246b0d518
Quim Robi Sermonia <quimrobi.s@gmail.com>
2022-07-17 10:51:12 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { Clients } from "./containers/Clients";
 import { Home } from "./containers/Home";
 import { Navigation } from "./components/Navigation";
 
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes>
         <Route element={<Navigation />}>
           <Route path="/" element={<Home />} />
