c6a21d283bc69b5a7b62cbc52266856e3b3a5a03
Quim Robi Sermonia <quimrobi.s@gmail.com>
2022-07-17 10:50:35 +0800

    Update Home UI

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -6,16 +5,16 @@ import Button from "@mui/material/Button";
 
 export const Home = () => {
   return (
-    <Box>
+    <Box textAlign="center">
       <Typography
         variant="h4"
         component="div"
-        sx={{ textAlign: "center", paddingX: 20 }}
+        sx={{ paddingX: 20, paddingTop: 15 }}
       >
         The Healthcare Platform you and your clients will love
       </Typography>
       <Link to="/clients">
-        <Button variant="contained" sx={{ mt: 3 }}>
+        <Button variant="contained" sx={{ marginTop: 3 }}>
           View Client List
         </Button>
       </Link>
